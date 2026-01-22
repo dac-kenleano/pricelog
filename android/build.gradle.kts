66b9e599de894545741a3db27793a4131aae536f
kenleano <kenleano@gmail.com>
2026-01-22 17:25:27 +0800

    fix for windows

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,3 +19,15 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+android {
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_17
+                targetCompatibility JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
